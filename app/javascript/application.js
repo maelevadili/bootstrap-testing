910eb3f334babf83d8c206e341150d1852bab3a7
MAELLA <mdegras@myrezapp.com>
2024-05-20 08:55:54 +0200

    Fixed issue with tooltips and popovers not working. Not sure it is the best way, though.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,3 +8,14 @@ import "bootstrap"
 // import "flatpickr"
 // import "simplebar"
 
+// Enable popovers everywhere
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+// Enable tooltips everywhere
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
