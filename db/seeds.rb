e742f74bc374e20b1d7b4d6a6a10a34484282991
MAELLA <mdegras@myrezapp.com>
2024-05-21 12:18:50 +0200

    Completed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'date'
+
+puts "Reboot..."
+
+Customer.destroy_all
+
+puts "Running seeds..."
+
+puts "Creating Customers..."
+
+d1 = DateTime.new(1986, 6, 6)
+d2 = DateTime.new(1977, 7, 2)
+d3 = DateTime.new(1958, 5, 25)
+d4 = DateTime.new(1956, 7, 2)
+d5 = DateTime.new(1993, 11, 17)
+
+Customer.create!(firstname: "Jane", lastname: "DOE",
+  email: "jane@unknown.com", phone: "+33690485590", date_of_birth: "d2")
+
+Customer.create!(firstname: "Bob", lastname: "SPONGE",
+  email: "bob@ocean.com", phone: "+33682940052", date_of_birth: "d3")
+
+Customer.create!(firstname: "Mickey", lastname: "MICE",
+  email: "mickey@disney.com", phone: "+33683905564", date_of_birth: "d4")
+
+Customer.create!(firstname: "Maëlla", lastname: "DÉGRAS",
+  email: "mdegras@myrezapp.com", phone: "+33600000000", date_of_birth: "d1")
+
+Customer.create!(firstname: "Mike", lastname: "TYSON",
+  email: "uppercut@mike.com", phone: "+33857894456", date_of_birth: "d5")
+
+puts "All done!"
