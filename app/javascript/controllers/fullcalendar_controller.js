fd43d753612dbe66d8e328847b17dab88b26ed81
MAELLA <mdegras@myrezapp.com>
2024-05-22 14:06:36 +0200

    played a bit with fullcalendar settings

--- app/javascript/controllers/fullcalendar_controller.js
+++ app/javascript/controllers/fullcalendar_controller.js
@@ -6,21 +6,29 @@ import { Calendar } from "fullcalendar"
 
 let events = [
   {
-    title: 'event1',
-    start: '2024-05-21',
-    end: '2024-05-22'
+    title: '24h event',
+    start: '2024-05-21T10:00:00',
+    end: '2024-05-22T10:00:00',
+    allDay: false
       },
   {
-    title: 'event2',
-    start: '2024-05-25',
-    end: '2024-05-28'
+    title: '3-day event',
+    start: '2024-05-25T00:00:00',
+    end: '2024-05-28T00:00:00', // Means 28 is excluded
+    allDay: false
       },
   {
     title: 'event3',
     start: '2024-05-29T12:30:00',
     end: '2024-05-29T14:30:00',
+    allDay: false
+      },
+  {
+    title: 'event4',
+    start: '2024-05-29T09:00:00',
+    end: '2024-05-29T11:00:00',
     allDay: false // will make the time show
-      }
+  }
 ];
 
 export default class extends Controller {
@@ -29,18 +37,19 @@ export default class extends Controller {
     var calendar = new Calendar(calendarEl, {
       // plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
       // Declare plugins is not needed.
-      // I guess they're automatically added from "fullcalendar"
+      // I guess they're automatically imported from "fullcalendar"
       // See https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.js
-      initialView: 'dayGridMonth',
+      initialView: 'timeGridWeek',
+      // initialView: 'dayGridMonth',
       // initialView: 'listMonth',
       // initialView: 'listWeek',
       // initialView: 'dayGridWeek',
       // initialView: 'dayGridDay',
-      defaultView: 'dayGridMonth',
-      header: {
-        left: 'prev,next today',
+      // defaultView: 'dayGridMonth',
+      headerToolbar: {
+        left: 'prev,next,today',
         center: 'title',
-        right: 'dayGridMonth,dayGridWeek,listMonth'
+        right: 'timeGridWeek,listWeek,timeGridDay,listDay,dayGridMonth' // user can switch between the two
       },
       events: events,
       eventColor: '#00d27a'
