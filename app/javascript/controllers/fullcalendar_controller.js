aaefbcfbfe06ba621fa60b8dcfd4cd60c3dd784f
MAELLA <mdegras@myrezapp.com>
2024-05-28 08:57:20 +0200

    Calendar view changer and selector work

--- app/javascript/controllers/fullcalendar_controller.js
+++ app/javascript/controllers/fullcalendar_controller.js
@@ -3,78 +3,93 @@
 import { Controller } from "@hotwired/stimulus"
 import { Calendar } from "fullcalendar"
 
-let events = [
+const events = [
   {
     title: '24h event',
-    start: '2024-05-21T10:00:00',
-    end: '2024-05-22T10:00:00',
+    start: '2024-05-28T10:00:00',
+    end: '2024-05-29T10:00:00',
     allDay: false
       },
   {
     title: '3-day event',
-    start: '2024-05-25T00:00:00',
-    end: '2024-05-28T00:00:00', // Means 28 is excluded
+    start: '2024-05-29T00:00:00',
+    end: '2024-05-31T00:00:00', // Means 31 is excluded
     allDay: false
       },
   {
     title: 'event3',
-    start: '2024-05-29T12:30:00',
-    end: '2024-05-29T14:30:00',
+    start: '2024-06-01T12:30:00',
+    end: '2024-06-01T14:30:00',
     allDay: false
       },
   {
     title: 'event4',
-    start: '2024-05-29T09:00:00',
-    end: '2024-05-29T11:00:00',
+    start: '2024-05-31T09:00:00',
+    end: '2024-05-31T11:00:00',
     allDay: false // will make the time show
   }
 ];
 
-// JUST TRYING SOMETHING, WORK IN PROGRESS. NEVERMIND FOR NOW.
-// let test = document.getElementById("calendar-filter").value;
-// console.log(test);
-
-// let userView = "";
-// console.log(userView);
-
-// let currentView = "timeGridWeek";
-
-// if (document.getElementById("calendar-filter").value == "Day view")
-//   currentView = 'timeGridDay'
-// else if (document.getElementById("calendar-filter").value == "List view")
-//   currentView = 'listWeek'
-// else if (document.getElementById("calendar-filter").value == "Month view")
-//   currentView = 'dayGridMonth'
-// else
-//   currentView = 'timeGridWeek'
-
-// currentView = currentView;
-
-// console.log(currentView)
-
-// let userView = function calendarViewChange() {
-//   if (document.getElementById("calendar-filter").value == "Day view") {
-//     userView = 'timeGridDay';
-//   }
-//   else if (document.getElementById("calendar-filter").value == "List view") {
-//     userView = 'listWeek';
-//   }
-//   else if (document.getElementById("calendar-filter").value == "Month view") {
-//     userView = 'dayGridMonth';
-//   }
-//   else {
-//     userView = 'timeGridWeek';
-//   }
-//   console.log(userView);
-// };
-
-// console.log(userView);
-
-// var utils = getData;
-
 export default class extends Controller {
   connect() {
     console.log("Hello from fullcalendar_controller.js");
+
+    var camelize = function camelize(str) {
+      var text = str.replace(/[-_\s.]+(.)?/g, function (match, capture) {
+        if (capture) {
+          return capture.toUpperCase();
+        }
+        return '';
+      });
+      return "".concat(text.substr(0, 1).toLowerCase()).concat(text.substr(1));
+    };
+    var getData = function getData(el, data) {
+      try {
+        return JSON.parse(el.dataset[camelize(data)]);
+      } catch (e) {
+        return el.dataset[camelize(data)];
+      }
+    };
+    var utils = {
+      camelize: camelize,
+      getData: getData
+    };
+
+    var Selectors = {
+      ACTIVE: '.active',
+      ADD_EVENT_FORM: '#addEventForm',
+      ADD_EVENT_MODAL: '#addEventModal',
+      CALENDAR: '.calendar',
+      CALENDAR_TITLE: '.calendar-title',
+      DATA_CALENDAR_VIEW: '[data-fc-view]',
+      DATA_EVENT: '[data-event]',
+      DATA_VIEW_TITLE: '[data-view-title]',
+      EVENT_DETAILS_MODAL: '#eventDetailsModal',
+      EVENT_DETAILS_MODAL_CONTENT: '#eventDetailsModal .modal-content',
+      EVENT_START_DATE: "#addEventModal [name='startDate']",
+      INPUT_TITLE: "[name='title']"
+    };
+    var Events = {
+      CLICK: 'click',
+      SHOWN_BS_MODAL: 'shown.bs.modal',
+      SUBMIT: 'submit'
+    };
+    var DataKeys = {
+      EVENT: 'event',
+      FC_VIEW: 'fc-view'
+    };
+    var ClassNames = {
+      ACTIVE: 'active'
+    };
+    var eventList = events.reduce(function (acc, val) {
+      if (val.schedules) {
+        return acc.concat(val.schedules.concat(val));
+      }
+      return acc.concat(val);
+    }, []);
+    var updateTitle = function updateTitle(title) {
+      document.querySelector(Selectors.CALENDAR_TITLE).textContent = title;
+    };
 
     var calendarEl = document.getElementById('calendar');
 
@@ -90,58 +105,78 @@ export default class extends Controller {
       // initialView: 'dayGridWeek',
       // initialView: 'dayGridDay',
       // defaultView: 'dayGridMonth',
-      headerToolbar: {
-        left: 'prev,next,today',
-        center: 'title',
-        right: 'timeGridWeek,listWeek,timeGridDay,listDay,dayGridMonth' // user can switch between the two
-      },
+      // headerToolbar: {
+      //   left: 'prev,next,today',
+      //   center: 'title',
+      //   right: 'timeGridWeek,listWeek,timeGridDay,listDay,dayGridMonth' // user can switch between the two
+      // },
+      headerToolbar: false,
       events: events,
-      eventColor: '#00d27a'
+      eventColor: '#00d27a',
+      eventTextColor: '#232E3C'
     });
     calendar.render();
 
-    // console.log(calendar.getOption('initialView'));
-    // calendar.changeView('timeGridDay');
-    // console.log(calendar.view.type);
-
-    // JUST TRYING SOMETHING, WORK IN PROGRESS. NEVERMIND FOR NOW.
-    // document.addEventListener('DOMContentLoaded', function () {
-    //   var calendarEl = document.getElementById('calendar2');
-
-    //   var calendar = new Calendar(calendarEl, {
-    //     // plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
-    //     // Declare plugins is not needed.
-    //     // I guess they're automatically imported from "fullcalendar"
-    //     // See https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.js
-    //     initialView: 'timeGridWeek',
-    //     // initialView: 'dayGridMonth',
-    //     // initialView: 'listMonth',
-    //     // initialView: 'listWeek',
-    //     // initialView: 'dayGridWeek',
-    //     // initialView: 'dayGridDay',
-    //     // defaultView: 'dayGridMonth',
-    //     headerToolbar: {
-    //       left: 'prev,next,today',
-    //       center: 'title',
-    //       right: 'timeGridWeek,listWeek,timeGridDay,listDay,dayGridMonth' // user can switch between the two
-    //     },
-    //     events: events,
-    //     eventColor: '#00d27a'
-    //   });
-    //   calendar.render();
-
-    //   // Get the dropdown element
-    //   var viewSelector = document.getElementById('calendar-view-selector');
-
-    //   // Add event listener for change event
-    //   viewSelector.addEventListener('change', function () {
-    //     var selectedView = this.value; // Get the selected view from the dropdown
-    //     calendar.changeView(selectedView); // Change the calendar view
-    //   });
-
-    //   calendar.render();
-    // });
-
-    // calendar.changeView('dayGridMonth');
+    updateTitle(calendar.currentData.viewTitle);
+    document.querySelectorAll(Selectors.DATA_EVENT).forEach(function (button) {
+      button.addEventListener(Events.CLICK, function (e) {
+        var el = e.currentTarget;
+        var type = utils.getData(el, DataKeys.EVENT);
+        switch (type) {
+          case 'prev':
+            calendar.prev();
+            updateTitle(calendar.currentData.viewTitle);
+            break;
+          case 'next':
+            calendar.next();
+            updateTitle(calendar.currentData.viewTitle);
+            break;
+          case 'today':
+            calendar.today();
+            updateTitle(calendar.currentData.viewTitle);
+            break;
+          default:
+            calendar.today();
+            updateTitle(calendar.currentData.viewTitle);
+            break;
+        }
+      });
+      document.querySelectorAll(Selectors.DATA_CALENDAR_VIEW).forEach(function (link) {
+        link.addEventListener(Events.CLICK, function (e) {
+          e.preventDefault();
+          var el = e.currentTarget;
+          var text = el.textContent;
+          el.parentElement.querySelector(Selectors.ACTIVE).classList.remove(ClassNames.ACTIVE);
+          el.classList.add(ClassNames.ACTIVE);
+          document.querySelector(Selectors.DATA_VIEW_TITLE).textContent = text;
+          calendar.changeView(getData(el, DataKeys.FC_VIEW));
+          updateTitle(calendar.currentData.viewTitle);
+        });
+      });
+      addEventForm && addEventForm.addEventListener(Events.SUBMIT, function (e) {
+        e.preventDefault();
+        var _e$target = e.target,
+          title = _e$target.title,
+          startDate = _e$target.startDate,
+          endDate = _e$target.endDate,
+          label = _e$target.label,
+          description = _e$target.description,
+          allDay = _e$target.allDay;
+        calendar.addEvent({
+          title: title.value,
+          start: startDate.value,
+          end: endDate.value ? endDate.value : null,
+          allDay: allDay.checked,
+          className: allDay.checked && label.value ? "bg-".concat(label.value, "-subtle") : '',
+          description: description.value
+        });
+        e.target.reset();
+        window.bootstrap.Modal.getInstance(addEventModal).hide();
+      });
+    })
+    addEventModal && addEventModal.addEventListener(Events.SHOWN_BS_MODAL, function (_ref15) {
+      var currentTarget = _ref15.currentTarget;
+      currentTarget.querySelector(Selectors.INPUT_TITLE).focus();
+    });
   }
 }
